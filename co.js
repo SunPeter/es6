9af95ff8d51422ac88b6e190eda3931c4bdeca88
SunPeter <ssd3237649@gmail.com>
2016-03-12 23:25:01 +0800

    update

--- co.js
+++ co.js
@@ -1,18 +1,49 @@
-var fs = require("fs")
+// http://www.ruanyifeng.com/blog/2015/05/thunk.html
+// var fs = require("fs")
 
-function ReadFile() {
-    return function(fn) {
-        fs.readFile("./generator.js", "utf8", function(err, data) {
-            fn(err, data)
-        })
-    }
+// function ReadFile() {
+//     return function(fn) {
+//         fs.readFile("./generator.js", "utf8", function(err, data) {
+//             fn(err, data)
+//         })
+//     }
+// }
+// var thunk = ReadFile();
+
+// thunk(function(err, data) {
+// 	if (err) {
+// 		console.log(err)
+// 	} else {
+// 		console.log(data)
+// 	}
+// });
+
+
+function* gen(){
+	yield 1;
+	yield 2;
+	yield 3;
+	return 4;
 }
-var thunk = ReadFile();
 
-thunk(function(err, data) {
-	if (err) {
-		console.log(err)
-	} else {
-		console.log(data)
-	}
+function co(fn) {
+  return function(done) {
+    var ctx = this;
+    var gen = fn.call(ctx);
+    var it = null;
+    function _next(err, res) {
+      it = gen.next(res);
+      if (it.done) {
+        done.call(ctx, err, it.value);
+      } else {
+        it.value(_next);
+      }
+    }
+    _next();
+  }
+}
+var g = new gen()
+var r = co(g)
+r(function(err, data){
+	console.log(data)
 });
